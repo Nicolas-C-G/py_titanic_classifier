4b21042091aaf3355d026067fb5ec1bfb1c84ca8
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-04-15 21:41:11 -0400

    Add command to get public IP

--- titanic/settings.py
+++ titanic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-8@o4s%kw+qb69bnkp@!d6i*p##85!dq6imtc1r3&@w@vne^8me
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["'"+os.system("curl http://checkip.amazonaws.com")+":8000'"]
 
 
 # Application definition
