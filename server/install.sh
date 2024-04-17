ea8876f9fbf5edb052f1248083eea322f4595381
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-04-16 21:56:44 -0400

    adjusting server files

--- server/install.sh
+++ server/install.sh
@@ -12,7 +12,7 @@ cd /home/ubuntu && git clone https://github.com/Nicolas-C-G/py-randomforest-samp
 cd /home/ubuntu/py-randomforest-sample && pip3 install -r requirements.txt
 python3 main.py
 
-#cd /home/ubuntu && git clone https://github.com/Nicolas-C-G/py_titanic_classifier.git
+cd /home/ubuntu && git clone https://github.com/Nicolas-C-G/py_titanic_classifier.git
 cd /home/ubuntu/py_titanic_classifier && rm titanic_model.sav
 cd /home/ubuntu/py-randomforest-sample
 cp titanic_model.sav /home/ubuntu/py_titanic_classifier/
